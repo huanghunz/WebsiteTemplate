5dad6abab7cfe058e8d058d4f327d50f537265dd
DESKTOP-7JO3UEO\huang <huanghunz@hotmail.com>
2018-10-29 22:05:59 -0700

    get the working version of angularfire2 and firebase module, init components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
-// import {AngularFireModule} from 'angularfire2';
-// import {AngularFireDatabaseModule} from 'angularfire2/database';
-// import {AngularFireAuthModule} from 'angularfire2/auth';
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+import {AngularFireAuthModule} from 'angularfire2/auth';
 
 import { AppComponent } from './app.component';
 import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { CheckOutComponent } from './check-out/check-out.component';
+import { OrderSucessComponent } from './order-sucess/order-sucess.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BsNavbarComponent
+    BsNavbarComponent,
+    HomeComponent,
+    ProductsComponent,
+    ShoppingCartComponent,
+    CheckOutComponent,
+    OrderSucessComponent
   ],
   imports: [
     BrowserModule,
-    // AngularFireModule.initializeApp(environment.firebase), // angular cli will choose
-    // AngularFireDatabaseModule,
-    // AngularFireAuthModule
-
+    AngularFireModule.initializeApp(environment.firebase), // angular cli will choose
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
